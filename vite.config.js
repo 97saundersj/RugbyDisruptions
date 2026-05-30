5c78d3ba604cfcd037127324da27efe08a322602
97saundersj <jacques_s@hotmail.co.uk>
2026-05-30 15:38:03 +0100

    Set base URL in Vite configuration to support environment variable

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [vue({ template: { transformAssetUrls } }), vuetify()],
 });
